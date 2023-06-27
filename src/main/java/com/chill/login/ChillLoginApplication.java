93c8a13103babc51f152fbd38a9069eb7038cba5
chill <zhouxy@zallsteel.com>
2023-06-27 10:25:21 +0800

    [build]chill:module init

---
+++ src/main/java/com/chill/login/ChillLoginApplication.java
@@ -0,0 +1,13 @@
+package com.chill.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChillLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chill/login/ChillLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chill.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChillLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
